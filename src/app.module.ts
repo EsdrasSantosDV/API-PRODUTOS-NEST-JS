6a3625c0edaee9381f7ea57037a4adbb6d9a2dea
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-08-04 19:35:11 -0300

    API FINALIZADA

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -10,12 +11,13 @@ import { ProdutosService } from './produtos.services';
 //PRECISA COLOCAR NO APP.MODULE AS CONTROLLERS E OS SERVICES
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
       dialect: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: 'Latveria67',
+      username: process.env.USUARIO_BANCO_DADOS,
+      password: process.env.SENHA_BANCO_DADOS,
       database: 'produtos',
       autoLoadModels:true,
       synchronize:true,
